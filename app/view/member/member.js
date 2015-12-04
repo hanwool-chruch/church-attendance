a8756a12a16097a468f09324572355ce85b5bb75
JEON YOUNG HO <gplusit@gmail.com>
2015-12-04 21:34:31 +0900

    'better'

--- app/view/member/member.js
+++ app/view/member/member.js
@@ -9,9 +9,13 @@ angular.module('myApp.member', ['ngRoute', 'ngResource'])
 		templateUrl: 'view/member/member.html',
 		controller: 'MemberCtrl'
 	})
-	.when('/member/:memberId', {
+	.when('/member/view/:memberId', {
 		templateUrl: 'view/member/memberDetail.html',
 		controller: 'MemberDetailCtrl'
+	})
+	.when('/member/regist', {
+		templateUrl: 'view/member/memberDetail.html',
+		controller: 'MemberRegistCtrl'
 	})
 	;
 }])
@@ -73,7 +77,11 @@ angular.module('myApp.member', ['ngRoute', 'ngResource'])
 	});
 	
 	$scope.detail = function(memberId){
-		$location.path('/member/'+memberId).search({});
+		$location.path('/member/view/'+memberId).search({});
+	}
+	
+	$scope.regist = function(){
+		$location.path('/member/regist');
 	}
 }])
 
@@ -90,7 +98,7 @@ angular.module('myApp.member', ['ngRoute', 'ngResource'])
 	
 	init();
 	
-	$q.all([MemberSvc.getDetail($routeParams.memberId), CodeSvc.getCodeList($routeParams.memberId)])
+	$q.all([MemberSvc.getDetail($routeParams.memberId), CodeSvc.getCodeList()])
 		
 	.then(function(resultArray){
 		
@@ -111,10 +119,68 @@ angular.module('myApp.member', ['ngRoute', 'ngResource'])
 	
 	/* 대원정보 저장 */
 	$scope.save = function(){
+			
+		if($scope.memberForm.$invalid) {
+			$.notify('이름 혹은 핸드폰 전화번호를 형식에 맞게 입력해주세요.');
+		} else {
+			
+			$rootScope.backdrop = 'backdrop';
+			
+			MemberSvc.save($scope.member).success(function(data){
+				
+				$rootScope.backdrop = undefined;
+				
+				$location.path('/member');
+				$.notify('저장되었습니다.');
+			});
+		}
+	}
+}])
+.controller('MemberRegistCtrl', ['$scope', '$rootScope', '$window', 'MemberSvc', '$location', 'CodeSvc', '$q',
+                        function ($scope ,  $rootScope ,  $window ,  MemberSvc ,  $location ,  CodeSvc ,  $q) {
+	
+	$rootScope.title = '대원관리';
+	$rootScope.title_icon = 'ion-person';
+	$rootScope.backdrop = 'backdrop';
+	
+	var init = function () {
+		selectMenu(1); /* 메뉴 선택 */
+	};
+	
+	init();
+	
+	$q.all([CodeSvc.getCodeList()])
 		
-		$rootScope.backdrop = 'backdrop';
+	.then(function(resultArray){
+		$scope.code = resultArray[0].data;
+		$rootScope.backdrop = undefined;
 		
-		if($scope.member.memberId) {
+		$scope.member = new Object();
+		
+		$scope.member.cPositionCd = $scope.code.cPositionList[0].C_POSITION_CD;
+		$scope.member.cPositionNm = $scope.code.cPositionList[0].C_POSITION_NM;
+		$scope.member.positionCd = $scope.code.positionList[$scope.code.positionList.length-1].POSITION_CD;
+		$scope.member.positionNm = $scope.code.positionList[$scope.code.positionList.length-1].POSITION_NM;
+		$scope.member.statusNm = $scope.code.statusList[0].STATUS_NM;
+		$scope.member.statusCd = $scope.code.statusList[0].STATUS_CD;
+		$scope.member.partNm = $scope.code.partList[0].PART_NM;
+		$scope.member.partCd = $scope.code.partList[0].PART_CD;
+	});
+	
+	/* 대원 목록으로 이동 버튼*/
+	$scope.gotoMemberList = function(){
+		$location.path('/member');
+	}
+	
+	/* 대원정보 저장 */
+	$scope.save = function(){
+			
+		if($scope.memberForm.$invalid) {
+			$.notify('이름 혹은 핸드폰 전화번호를 형식에 맞게 입력해주세요.');
+		} else {
+			
+			$rootScope.backdrop = 'backdrop';
+			
 			MemberSvc.save($scope.member).success(function(data){
 				
 				$rootScope.backdrop = undefined;
