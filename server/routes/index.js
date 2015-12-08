5277e73c7a60e9eabedc22a4ac3d825e2af29a1d
JEON YOUNG HO <gplusit@gmail.com>
2015-12-08 13:34:17 +0900

    'better'

--- server/routes/index.js
+++ server/routes/index.js
@@ -18,13 +18,13 @@ function handleDisconnect() {
 	db = mysql.createConnection(db_config);
 	db.connect(function(err) {
 		if (err) {
-			console.log('error when connecting to db:', err);
+			//console.log('error when connecting to db:', err);
 			setTimeout(handleDisconnect, 2000);
 		}
 	});
 	
 	db.on('error', function(err) {
-		console.log('db error', err);
+		//console.log('db error', err);
 		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
 			handleDisconnect();
 		} else {
