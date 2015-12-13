db6fec0ad460e7c0ae7d6c73eaefd647ccbb763a
JEON YOUNG HO <gplusit@gmail.com>
2015-12-14 00:17:02 +0900

    build structure for unit testing

--- app/view/rank/rank_test.js
+++ app/view/rank/rank_test.js
@@ -8,7 +8,7 @@ describe('myApp.rank module', function() {
 
 		it('should ....', inject(function($controller, $rootScope, RankSvc) {
 			// spec body
-			var rankCtrl = $controller('RankCtrl', {$scope: $rootScope.$new(), $rootScope: $rootScope ,  RankSvc: RankSvc});
+			var rankCtrl = $controller('RankCtrl', {$scope: $rootScope.$new(), $rootScope: $rootScope, RankSvc: RankSvc});
 			expect(rankCtrl).toBeDefined();
 		}));
 
