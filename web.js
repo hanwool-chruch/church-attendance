69196f3aa65e947273109a03c25816a1131209ca
JEON YOUNG HO <gplusit@gmail.com>
2015-12-16 18:32:32 +0900

    remove socket's log level option on web.js

--- web.js
+++ web.js
@@ -51,7 +51,6 @@ app.get		('/rest/doc/:docId', routes.modifyDoc);
 
 /* 웹소켓 */
 var io = socketio.listen(server);
-io.set('log level',1);
 io.on('connection',function(socket) {
 
 	/* 연습정보 상세정보 입장 */
